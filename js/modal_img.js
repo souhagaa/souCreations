d1ac6da36415c931f882baaf07e89663ad045a2b
souha <souha.gaaloul@hotmail.com>
2020-10-19 00:15:17 +0200

    about us done

--- js/modal_img.js
+++ js/modal_img.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+/*var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
@@ -19,3 +19,35 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}*/
+
+// Get the modal
+  var modal = document.getElementById("first-modal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("first-work");
+  var modalImg = document.getElementById("modal-content0");
+  var captionText = document.getElementById("caption");
+
+  img.onclick = function(captionText){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = captionText.value;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+  modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
